cd8035524038fe2125afa1dbdad760ba3e70f0d8
d3v-tushar <d3v.tushar@gmail.com>
2022-10-10 04:34:05 +0600

    React Router Setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../images/Logo.svg'
 import './Header.css';
 
@@ -7,9 +8,10 @@ const Header = () => {
             <nav className='header'>
                 <img src={logo} alt="" />
                 <div>
-                    <a href="/home">Home</a>
-                    <a href="/orders">Orders</a><a href="/Inventory">Inventory</a>
-                    <a href="/about">About</a>
+                    <Link to="/">Shop</Link>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/Inventory">Inventory</Link>
+                    <Link to="/about">About</Link>
                 </div>
             </nav>
     );
