1ad93d28998452f334bd5a91f4193971e88cc4a0
d3v-tushar <d3v.tushar@gmail.com>
2022-10-14 06:06:38 +0600

    Remove Product From Card Added in Order Page

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
+import ReviewItems from '../ReviewItems/ReviewItems';
 
 const Orders = () => {
     const {products, initialCart} = useLoaderData();
     const [cart, setCart] = useState(initialCart);
+
+    const handleRemoveItem = (id) => {
+        console.log(id);
+        const remainning = cart.filter(product => product.id !== id);
+        console.log(remainning);
+        setCart(remainning);
+        removeFromDb(id);
+    }
     return (
         <div className='shop-container'>
-            <div className='products-container'>
-
+            <div className='orders-container'>
+                {
+                    cart.map(product => <ReviewItems key={product.id} product={product}></ReviewItems>)
+                }
             </div>
             <div className='cart-container'>
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart} handleRemoveItem={handleRemoveItem}></Cart>
             </div>
         </div>
     );
